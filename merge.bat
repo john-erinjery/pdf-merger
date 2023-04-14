2de946308a861d34be8a9fddbdd506b2b2771b31
John Erinjery <91457967+john-erinjery@users.noreply.github.com>
2023-04-14 20:19:10 +0530

    Update merge.bat

--- merge.bat
+++ merge.bat
@@ -1,1 +1,2 @@
-C:\Programing\python-pdf\env\Scripts\python.exe C:\Programing\python-pdf\pdf-merger\merge.py
+<python-path> <merge.py-path>
+:: eg: (you can dlt this) C:\Programing\python-pdf\env\Scripts\python.exe C:\Programing\python-pdf\pdf-merger\merge.py
